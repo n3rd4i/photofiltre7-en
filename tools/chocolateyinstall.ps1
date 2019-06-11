e260e0f47f35e756cf14cf2f58a8ce39d05846af
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-06-11 10:00:03 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://static.infomaniak.ch/photofiltre/utils/pf7/pf7-setup-en-7.2.1.exe' # download url, HTTPS preferred
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'photofiltre7-en*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+ 
+Install-ChocolateyPackage @packageArgs
